f6a5de886d6d792c99d1b2d35f5402db11eea664
danielgripa <112982758+danielgripa@users.noreply.github.com>
2024-11-20 19:15:10 -0300

    removido os dotenvs

--- src/main/java/br/edu/ibmec/projeto_cloud/ProjetoCloudApplication.java
+++ src/main/java/br/edu/ibmec/projeto_cloud/ProjetoCloudApplication.java
@@ -7,12 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetoCloudApplication {
 
     public static void main(String[] args) {
-        System.setProperty("DB_HOST", System.getenv("DB_HOST"));
-        System.setProperty("DB_PORT", System.getenv("DB_PORT"));
-        System.setProperty("DB_NAME", System.getenv("DB_NAME"));
-        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-
-        SpringApplication.run(ProjetoCloudApplication.class, args);
     }
 }
