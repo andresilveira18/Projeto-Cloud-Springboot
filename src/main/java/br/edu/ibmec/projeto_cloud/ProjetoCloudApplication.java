45881fdb31c045149f136d9d9ae9821b4ac3ad4c
André Silveira <Andre>
2024-11-20 14:30:49 -0300

    atualizado para azure

--- src/main/java/br/edu/ibmec/projeto_cloud/ProjetoCloudApplication.java
+++ src/main/java/br/edu/ibmec/projeto_cloud/ProjetoCloudApplication.java
@@ -1,25 +1,18 @@
 package br.edu.ibmec.projeto_cloud;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoCloudApplication {
 
-	public static void main(String[] args) {
-		// Load .env only if not running under the 'test' profile
-		String activeProfile = System.getProperty("spring.profiles.active");
-		if (!"test".equals(activeProfile)) {
-			Dotenv dotenv = Dotenv.configure()
-					.directory("./Projeto-Cloud-Springboot")
-					.load();
-			System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-			System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-			System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		}
-		SpringApplication.run(ProjetoCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("DB_HOST", System.getenv("DB_HOST"));
+        System.setProperty("DB_PORT", System.getenv("DB_PORT"));
+        System.setProperty("DB_NAME", System.getenv("DB_NAME"));
+        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+
+        SpringApplication.run(ProjetoCloudApplication.class, args);
+    }
 }
